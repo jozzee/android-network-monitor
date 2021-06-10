4ef138166632a6852fbba8a526344f4dd68a54bc
Sitthiphong Muntrima <sitthiphong.mun@gmail.com>
2021-06-10 12:32:08 +0700

    - Publish to maven at version 1.0.0

--- sample/src/main/java/com/jozzee/android/networkmonitor/sample/MainActivity.kt
+++ sample/src/main/java/com/jozzee/android/networkmonitor/sample/MainActivity.kt
@@ -4,8 +4,8 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import androidx.lifecycle.Observer
-import com.jozzee.android.networkmonitor.NetworkMonitor
-import com.jozzee.android.networkmonitor.NetworkStatus
+import com.github.jozzee.android.networkmonitor.NetworkMonitor
+import com.github.jozzee.android.networkmonitor.NetworkStatus
 
 class MainActivity : AppCompatActivity() {
 
